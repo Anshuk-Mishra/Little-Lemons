6d5f788399ea3506cb4346e171fb1290dc53b1bf
Anshuk-Mishra <77469638+Anshuk-Mishra@users.noreply.github.com>
2023-05-09 01:56:59 +0530

    version 1_2

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,19 @@ from django.forms import ModelForm
 from .models import Book
 
 class formBook(ModelForm):
+
     class Meta:
         model = Book
         fields = "__all__"
+        require_css_class = 'required-field'
+        widgets = {
+            'Fname': forms.TextInput(attrs={'placeholder': 'John','class':'wi50'}),
+            'Lname': forms.TextInput(attrs={'placeholder': 'Williams'}),
+            'date': forms.TextInput(attrs={'placeholder': '06/11/2023'}),
+        }
+        labels = {
+            'Fname': 'First Name',
+            'Lname': 'Last Name',
+            'date': 'Date [mm/dd/yyyy]',
+            'time': 'Time Slot'
+        }
